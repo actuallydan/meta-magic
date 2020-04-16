5816863866fd59fe24b934bbab6df4ac2bcbf4fe
actuallyDan <dankral01@gmail.com>
2020-04-16 04:17:05 -0400

    Updates

---
+++ precache-manifest.de85b4c17a1570da3d9fd7ca8188bc01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c5886b2d8d9d7762ce",
+    "url": "/spell-slinger/static/js/app.0b3c832a.chunk.js"
+  },
+  {
+    "revision": "87727368cff28d817a22",
+    "url": "/spell-slinger/static/js/runtime~app.7b0b240f.js"
+  },
+  {
+    "revision": "283d56e672973f875318",
+    "url": "/spell-slinger/static/js/2.4e47dff5.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/spell-slinger/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/spell-slinger/expo-service-worker.js"
+  },
+  {
+    "revision": "be10570191b0273f089cf02db82361cf",
+    "url": "/spell-slinger/index.html"
+  },
+  {
+    "revision": "4443259adc9772d31b21f7f18db27363",
+    "url": "/spell-slinger/manifest.json"
+  },
+  {
+    "revision": "0feab5ffe273f6cf9dd69b6afea1efd0",
+    "url": "/spell-slinger/register-service-worker.js"
+  }
+];
